c0c9f96a064869e21b374a1c33ac3828f612b5de
Rebirator <terretoriatitanx@gmail.com>
2021-10-02 17:32:19 +0300

    .

--- CS GO First Internal/Main/main.cpp
+++ CS GO First Internal/Main/main.cpp
@@ -13,17 +13,23 @@ DWORD WINAPI HackThread( HMODULE hModule )
     freopen_s( ( FILE** )stdin, "CONIN$", "r", stdout );
     freopen_s( ( FILE** )stdin, "CONOUT$", "w", stdout );
 
+    auto start_time = std::chrono::system_clock::now( );
+
     uintptr sig_test = g_Signature->Scan( L"client.dll", { 0x89, 0x0D, -1, -1, -1, -1, 0x8B, 0x0D, -1, -1, -1, -1, 0x8B, 0xF2, 0x8B, 0xC1, 0x83, 0xCE, 0x04 }, 2 );
-    std::cout << std::hex << std::uppercase << sig_test << std::endl;
+    std::cout << std::hex << std::uppercase << "dwForceAttack offset: " << sig_test << std::endl;
 
+    auto end_time = std::chrono::system_clock::now( );
+    std::cout << "Signature parsed in: " << std::chrono::duration< double >( end_time - start_time ).count( ) << std::endl;
+
+    // Main loop
     while ( !GetAsyncKeyState( VK_INSERT ) ) {
-
         // Cheat
         Hack( );
 
-        //std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
     }
 
+    // Exit
     fclose( stdin );
     fclose( stdout );
     FreeConsole( );
