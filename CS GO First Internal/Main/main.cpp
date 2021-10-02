2135b4c0978f252c6fdb757e777fc5434b853076
Rebirator <terretoriatitanx@gmail.com>
2021-10-02 17:36:02 +0300

    .

--- CS GO First Internal/Main/main.cpp
+++ CS GO First Internal/Main/main.cpp
@@ -16,10 +16,10 @@ DWORD WINAPI HackThread( HMODULE hModule )
     auto start_time = std::chrono::system_clock::now( );
 
     uintptr sig_test = g_Signature->Scan( L"client.dll", { 0x89, 0x0D, -1, -1, -1, -1, 0x8B, 0x0D, -1, -1, -1, -1, 0x8B, 0xF2, 0x8B, 0xC1, 0x83, 0xCE, 0x04 }, 2 );
-    std::cout << std::hex << std::uppercase << "dwForceAttack offset: " << sig_test << std::endl;
+    std::cout << std::hex << std::uppercase << "dwForceAttack offset: 0x" << sig_test << std::endl;
 
     auto end_time = std::chrono::system_clock::now( );
-    std::cout << "Signature parsed in: " << std::chrono::duration< double >( end_time - start_time ).count( ) << std::endl;
+    std::cout << "Signature parsed in:   " << std::chrono::duration< double >( end_time - start_time ).count( ) << std::endl;
 
     // Main loop
     while ( !GetAsyncKeyState( VK_INSERT ) ) {
